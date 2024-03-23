fc1777c23a196afe3da6185045d300a65d8b5671
Elbert Nathanael <elbertwithane@gmail.com>
2024-03-23 19:06:22 +0800

    added server url

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import DataManager from './modules/dataManager'
 
 // --- start data manager
 const isDevelopment = true;
-const baseUrl = "http://localhost:8000/hangouts"
+// const baseUrl = "http://localhost:8000/hangouts" // local server
+const baseUrl = "http://192.53.113.254:3535/hangouts"
 DataManager.init(isDevelopment, baseUrl);
 
 // --- start svelte
