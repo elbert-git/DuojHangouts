e48c0bc94709b7a3b186ce0b4f38a438b686fc57
Elbert Nathanael <elbertwithane@gmail.com>
2024-05-25 13:33:47 +0800

    fixed cors

--- duojHangouts/settings.py
+++ duojHangouts/settings.py
@@ -19,9 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(f4n!j#3ao($^v^tnvul-c!7r7jrwzilgj+i_rp43o4bz(!^_8'
 
+# --- Dealing with CORS
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 # CORS_ALLOW_ALL_ORIGINS = DEBUG
